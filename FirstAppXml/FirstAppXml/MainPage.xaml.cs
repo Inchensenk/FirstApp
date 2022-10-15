310cbd6a13b51360d2941b135db1e6d4d16712f6
sofoyan gevork <aab81@bk.ru>
2022-10-15 19:22:07 +0300

    Создание приложения: Верстка интерфейса XAML Xamarin Forms (UI Xaml creation) #5
    https://www.youtube.com/watch?v=RLTaxmcHYR4&list=PLH3y3SWteZd2xUX46npPFspQar5BTHUIh&index=5

---
+++ FirstAppXml/FirstAppXml/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstAppXml
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            //чекбокс сразу будет проставлен
+            checkBox1.IsChecked = true; 
+        }
+    }
+}
