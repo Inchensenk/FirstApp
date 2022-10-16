3c593401bde369e41fa0040c1bb20df888d24cac
sofoyan gevork <aab81@bk.ru>
2022-10-16 11:00:03 +0300

    Позиционирование элементов на экране Xamarin Forms. Размер и положение #8
    https://www.youtube.com/watch?v=GqYXswK-bf4&list=PLH3y3SWteZd2xUX46npPFspQar5BTHUIh&index=8

---
+++ AlignmentApp/AlignmentApp/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlignmentApp
+{
+    /*
+     <StackLayout Margin="15">
+
+        <Label Text="Слева"
+               BackgroundColor="Gray"
+               HorizontalOptions="Start"/> --><!--HorizontalOptions="Start": Элемент притянут к левому краю экрана--><!--
+
+        <Label Text="По центру"
+               BackgroundColor="Gray"
+               HorizontalOptions="Center"/> --><!--HorizontalOptions="Center": Центрует элемент по середине--><!--
+
+        <Label Text="Справа"
+               BackgroundColor="Gray"
+               HorizontalOptions="End"/>   --><!--HorizontalOptions="End": Элемент притянут к правому краю экрана--><!--
+
+        <Label Text="Растянуть по Ox"
+               BackgroundColor="Gray"
+               HorizontalOptions="Fill"/>  --><!--HorizontalOptions="Fill": Элемент растянут на всю ширину родительского контейнера--><!--
+
+    </StackLayout>
+
+    Реализация тоже самого, но в C#. в Методе OnAppearing()
+     */
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// обрабатываем события создания событий
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            /*//горизонтальный стек
+            StackLayout stackLayout2 = new StackLayout()
+            {
+                Margin = new Thickness(15),
+                Orientation = StackOrientation.Horizontal,
+                Children =
+                {
+                    new Label {Text= "Слева", BackgroundColor = Color.Gray, VerticalOptions = LayoutOptions.Start},
+                    new Label {Text= "По центру", BackgroundColor = Color.Gray, VerticalOptions = LayoutOptions.Center},
+                    new Label {Text= "Справа", BackgroundColor = Color.Gray, VerticalOptions = LayoutOptions.End},
+                    new Label {Text= "На всю ширину", BackgroundColor = Color.Gray, VerticalOptions = LayoutOptions.Fill}
+                }
+            };
+            Content = stackLayout2;*/
+
+
+            /*
+            //Вертикальный стек
+            StackLayout stackLayout1 = new StackLayout()
+            {
+                Margin = new Thickness(15),
+                Children =
+                {
+                    new Label {Text= "Слева", BackgroundColor = Color.Gray, HorizontalOptions = LayoutOptions.Start},
+                    new Label {Text= "По центру", BackgroundColor = Color.Gray, HorizontalOptions = LayoutOptions.Center},
+                    new Label {Text= "Справа", BackgroundColor = Color.Gray, HorizontalOptions = LayoutOptions.End},
+                    new Label {Text= "На всю ширину", BackgroundColor = Color.Gray, HorizontalOptions = LayoutOptions.Fill}
+                }
+            };
+            Content = stackLayout1;*/
+        }
+    }
+}
